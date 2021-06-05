349fb5efefe6a394e3b60c53535aa7e0cc2030ba
kevin yao <1343406119@qq.com>
2021-06-05 21:44:29 +0800

    june_6.2

--- matlab_program/liziqunsuanfa/int.m
+++ matlab_program/liziqunsuanfa/int.m
@@ -1,17 +1,20 @@
-function y=int(x)
+function dif_int=int(x)
 global gama1 gama2
- delt_err = 2e-2;
+%delt_err = 2e-2;
 n = length(x(:,1));
-y = zeros(n,1)+1;
+dif_int = zeros(n,1);
     for i = 1:n
         gama1 = x(i,1);
         gama2 = x(i,2);
         pause(0.01);
         sim('origin_cont.slx'); 
-        inf = length(fm.data)-1;
+        inf = length(mfi.data)-1;
+        dif_int(i) = abs(rfi.data(inf)-1.02*mfi.data(inf));
+%        inf = length(fm.data)-1;
 %         y = abs(dec_ar.data(inf)+inc_ar.data(inf));
-        [f_infty,tp,ts,sigma]=para_cal(fm,delt_err);
-        y=log(ts/0.7+1)+log(sigma/0.1+1);
+
+%         [f_infty,tp,ts,sigma]=para_cal(fm,delt_err);
+%         y=log(ts/0.7+1)+log(sigma/0.1+1);
             
     end
 end
