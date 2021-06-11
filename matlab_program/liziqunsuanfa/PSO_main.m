36b9f03f8d673e1751c9c02d963a85128244d2ed
kevin yao <1343406119@qq.com>
2021-06-11 20:20:10 +0800

    粒子群优化曲面start

--- matlab_program/liziqunsuanfa/PSO_main.m
+++ matlab_program/liziqunsuanfa/PSO_main.m
@@ -1,17 +1,19 @@
-global gama1 gama2 ke
-bd = 300;
+global gama1 gama2 ke P I D
+bd = 500;
 kd = 1000;
 ke = 1000;
 ke_0 = 500;
-
+P = 1;
+I = 1;
+D = 1;
 
 f = @int;
 M = 20;
-data_f = zeros(M,2);
+data_f = zeros(M,5);
 for j = 1:M
     j
     ke = 1000+M*100;
-    dimension = 2; n = 40; m = 35; xmax = 500; xmin = 0; vmax = 30; vmin = -30;
+    dimension = 5; n = 30; m = 40; xmax = 600; xmin = 5; vmax = 30; vmin = -30;
     [Pg,fmin] = PSO(f,dimension,n,m,xmax,xmin,vmax,vmin)
     data_f(j,:) = Pg;
 end
--- matlab_program/liziqunsuanfa/int.m
+++ matlab_program/liziqunsuanfa/int.m
@@ -1,21 +1,21 @@
-function dif_int=int(x)
-global gama1 gama2
-%delt_err = 2e-2;
+function y=int(x)
+global gama1 gama2 P I D
+delt_err = 1e-2;
 n = length(x(:,1));
-dif_int = zeros(n,1);
+y = zeros(n,1);
     for i = 1:n
         gama1 = x(i,1);
         gama2 = x(i,2);
-        pause(0.01);
+        P = x(i,3);
+        I = x(i,4);
+        D = x(i,5);
         sim('origin_cont.slx'); 
-        inf = length(mfi.data)-1;
-        dif_int(i) = abs(rfi.data(inf)-1.02*mfi.data(inf));
+%         inf = length(mfi.data)-1;
+%         dif_int(i) = abs(rfi.data(inf)-1.012*mfi.data(inf));
 %        inf = length(fm.data)-1;
 %         y = abs(dec_ar.data(inf)+inc_ar.data(inf));
-
-%         [f_infty,tp,ts,sigma]=para_cal(fm,delt_err);
-%         y=log(ts/0.7+1)+log(sigma/0.1+1);
-            
+        [f_infty,tp,ts,sigma]=para_cal(fm,delt_err);
+        y(i)=log(ts/0.3+1)+log(sigma/0.5+1);
     end
 end
 
